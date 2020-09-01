0a5c8986c02c6358579c9177786ad3f00671420f
Kornel Skorka <kornel.skorka@allegro.pl>
2020-09-01 23:09:30 +0200

    init

---
+++ src/main/kotlin/pl/edu/pw/mini/velobackend/VeloBackendApplication.kt
@@ -0,0 +1,11 @@
+package pl.edu.pw.mini.velobackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VeloBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<VeloBackendApplication>(*args)
+}
---
+++ src/test/kotlin/pl/edu/pw/mini/velobackend/VeloBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package pl.edu.pw.mini.velobackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VeloBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
