ad4dfd074fd5fa41d7b93d0c043b5f00bb433f0e
Kornel Skorka <kornel.skorka@allegro.pl>
2020-12-06 19:38:43 +0100

    VELO-107 Athlete removal endpoint DELETE /athlete/remove-coach

--- src/main/kotlin/pl/edu/pw/mini/velobackend/api/athlete/AthleteEndpoint.kt
+++ src/main/kotlin/pl/edu/pw/mini/velobackend/api/athlete/AthleteEndpoint.kt
@@ -1,10 +1,13 @@
 package pl.edu.pw.mini.velobackend.api.athlete
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 import pl.edu.pw.mini.velobackend.domain.athlete.AthleteService
 import java.security.Principal
 import java.util.UUID
@@ -19,6 +22,15 @@ class AthleteEndpoint(
     fun addCoach(@RequestHeader coachEmail: String, principal: Principal) {
         val athleteEmail = principal.name
         athleteService.addAthleteForUserWithEmail(coachEmail, athleteEmail)
+    }
+
+    @DeleteMapping("/remove-coach")
+    fun removeCoach(@RequestHeader coachEmail: String, principal: Principal) {
+        val athleteEmail = principal.name
+        val result = athleteService.removeAthleteForUserWithEmail(coachEmail, athleteEmail)
+        if (!result.success) {
+            throw ResponseStatusException(HttpStatus.NOT_FOUND, result.message)
+        }
     }
 
     @GetMapping(produces = ["application/json"])
